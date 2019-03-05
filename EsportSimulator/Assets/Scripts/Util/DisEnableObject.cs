22ab1fec53d36aface8f4d29568f53fa5536dac4
wesleycats <wesleycats@gmail.com>
2019-03-05 15:02:21 +0300

    Added dis- / enable script

---
+++ EsportSimulator/Assets/Scripts/Util/DisEnableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisEnableObject : MonoBehaviour
+{
+    public void Disable(bool enable)
+    {
+        this.gameObject.SetActive(enable);
+    }
+}
