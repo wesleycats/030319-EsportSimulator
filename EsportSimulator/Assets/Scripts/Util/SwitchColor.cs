e72cd883d8e984a16d7c32ea73b60a48855734ec
wesleycats <wesleycats@gmail.com>
2019-03-20 13:17:47 +0300

    Added color switch script

---
+++ EsportSimulator/Assets/Scripts/Util/SwitchColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SwitchColor : MonoBehaviour
+{
+    [SerializeField] private Color switchTo;
+
+    private Image image;
+    private Color initialColor;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        initialColor = image.color;
+    }
+
+    public void Switch()
+    {
+        if (image.color == initialColor)
+            image.color = switchTo;
+        else
+            image.color = initialColor;
+    }
+}
